c8ea6d88e1c280e49fba572afbb010ce4bcec350
Lans12345 <olanalans12345@gmail.com>
2024-02-10 08:49:18 +0800

    added firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:qr_scanner/screens/splash_screen.dart';
 
-void main() {
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    name: 'amusement-2e6a5',
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
